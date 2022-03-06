20a4510693220ff284fc68d6fca0bcfeb34660cd
D3SOX <d3sox@protonmail.com>
2022-03-06 16:55:33 +0100

    feat: make folders selectable

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,4 +1,4 @@
-import { Divider, Navbar, ScrollArea } from '@mantine/core';
+import { ActionIcon, Divider, MantineColor, Navbar, ScrollArea } from '@mantine/core';
 import FolderButton from './input/FolderButton';
 import {
   ChevronRightIcon,
@@ -7,53 +7,66 @@ import {
   SyncIcon,
   XIcon,
 } from '@primer/octicons-react';
+import { useMemo, useState } from 'react';
 
 interface Folder {
+  id: number;
   name: string;
-  color: string;
-  selected?: boolean;
+  color: MantineColor;
 }
 
-const folders: Folder[] = [
-  {
-    name: 'Sounds 1',
-    color: 'blue',
-  },
-  {
-    name: 'Sounds 2',
-    color: 'yellow',
-  },
-  {
-    name: 'Sounds 3',
-    color: 'green',
-    selected: true,
-  },
-];
-
-// for (let i = 0; i < 999; i++) {
-//   folders.push({
-//     name: `Sounds ${i}`,
-//     color: 'red',
-//   });
-// }
-
 function AppNavbar() {
+  const [selectedFolder, setSelectedFolder] = useState(1);
+  const [folders, setFolders] = useState<Folder[]>([]);
+
+  useMemo(() => {
+    setFolders(
+      new Array(20).fill(0).map((_, i) => ({
+        id: i,
+        name: `Folder ${i + 1}`,
+        color: i % 2 === 0 ? 'red' : 'blue',
+      }))
+    );
+  }, []);
+
+  const removeFolder = (folder: Folder) => {
+    setFolders(folders.filter(({ id }) => id !== folder.id));
+    if (folders.length) {
+      setSelectedFolder(folders[0].id);
+    }
+  };
+
   return (
     <>
       <Navbar.Section grow mr={-10} sx={{ paddingRight: 10, paddingBottom: 10 }} component={ScrollArea}>
-        {folders.map((folder, index) => (
+        {folders.map(folder => (
           <FolderButton
-            key={index}
+            key={folder.id}
             icon={<FileDirectoryFillIcon />}
             color={folder.color}
             label={folder.name}
-            selected={folder?.selected}
+            selected={selectedFolder === folder.id}
             endIcon={
               <>
-                <SyncIcon />
-                <XIcon />
+                <ActionIcon component="div" variant="transparent" size="xs">
+                  <SyncIcon />
+                </ActionIcon>
+                <ActionIcon<'div'>
+                  component="div"
+                  variant="transparent"
+                  size="xs"
+                  onClick={event => {
+                    event.stopPropagation();
+                    removeFolder(folder);
+                  }}
+                >
+                  <XIcon />
+                </ActionIcon>
               </>
             }
+            onClick={() => {
+              setSelectedFolder(folder.id);
+            }}
           />
         ))}
       </Navbar.Section>
