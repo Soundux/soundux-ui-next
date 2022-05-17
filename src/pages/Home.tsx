321529df12cd4c2ea02d9c64081b729580a5bf61
D3SOX <nico@d3sox.me>
2022-05-17 15:36:52 +0200

    fix(home): handle no selected folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import ListView from '../components/views/ListView';
 import BottomBar from '../components/BottomBar';
+import { useAtom } from 'jotai';
+import { selectedFolderAtom } from '../store';
+import { Center, Text } from '@mantine/core';
 
 function Home() {
+  const [selectedFolder] = useAtom(selectedFolderAtom);
+
   return (
     <>
-      <ListView />
+      {selectedFolder !== null ? (
+        <ListView />
+      ) : (
+        <Center>
+          <Text>No folder selected</Text>
+        </Center>
+      )}
       <BottomBar />
     </>
   );
